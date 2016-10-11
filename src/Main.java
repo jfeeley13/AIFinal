db9a97875d491cc275fa5d7187a5b8715f922e2e
Kunal Shah <kkshah@wpi.edu>
2016-10-10 22:27:47 -0400

    Made a separate temporary package with some maze building magic from scratch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Maze obj=new Maze(61);
+        Maze obj=new Maze(5);
+   
+        obj.draw();
+        obj.solve();
     }
 }
